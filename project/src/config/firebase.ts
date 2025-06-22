480a3b1cf668d5a0890111b70b60b76b4e99d1d2
Brajendra1190 <brajendrasinghyadav8112@gmail.com>
2025-06-22 23:40:10 +0530

    fix(firebase): update authDomain to fin247.vercel.app and register in Firebase console

--- project/src/config/firebase.ts
+++ project/src/config/firebase.ts
@@ -4,8 +4,8 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBD3VHv0qsSWFT2owNNNQpIjA6kY3GR3Oc",
-  authDomain: "fin247.vercel.app",
-  projectId: "chai-61ebd",
+  authDomain: "chai-61ebd.firebaseapp.com",
+  projectId: "chai-61ebchai-61ebd",
   storageBucket: "chai-61ebd.appspot.com",
   messagingSenderId: "907556775279",
   appId: "1:907556775279:web:48d115f85983bc3060749a",
